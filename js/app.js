e1bc6fe13a969e38cb388b7f285df0531c48d997
Kevin McAmis <kevinluther@live.com>
2018-07-10 02:09:55 -0400

    Added player+enemy starting locations and sprites

--- js/app.js
+++ js/app.js
@@ -1,11 +1,20 @@
+document.onmousemove = function(e){
+var x = e.pageX;
+var y = e.pageY;
+e.target.title = "X is "+x+" and Y is "+y;
+}
+
 // Enemies our player must avoid
-let Enemy = function() {
+let Enemy = function(Speed, xEStartingPoint, yEStartingPoint) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
+    this.x = xEStartingPoint;
+    this.y = yEStartingPoint;
+    this.speed = Speed;
 };
 
 // Update the enemy's position, required method for game
@@ -14,6 +23,14 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+    // minSpeed = 50
+    maxSpeed = 350
+    this.x += this.speed * dt * Math.floor(Math.random() * (maxSpeed - this.speed) + this.speed);
+    // need to work on random speed so the minSpeed  actaully sets at 50 and maxSpeed is 350 when including dt
+
+    if (this.x >505) {
+      this.x = -500;
+    };
 };
 
 // Draw the enemy on the screen, required method for game
@@ -24,8 +41,10 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
-let Player = function() {
+let Player = function(xPStartingPoint, yPStartingPoint) {
   this.sprite = 'images/char-boy.png';
+  this.x = xPStartingPoint;
+  this.y = yPStartingPoint;
 };
 
 Player.prototype.update = function(dt) {};
@@ -37,8 +56,20 @@ Player.prototype.render = function() {
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 
-const player = Player;
-const allEnemies = [Enemy];
+// Player should start at x,y cordinates of 200,400, tihs puts them in the middle of the game board
+const player = new Player(200, 400);
+const allEnemies = [];
+// Enemy speed should be capped at 350 and min 50, so they are neither to fast or to slow
+// Enemy min y axis should be 300, or it starts in the player starting area
+// const enemy = new Enemy(1, 1, 300);
+// allEnemies.push(enemy); //tester code
+
+for (let i = 0; i < 10; i++) {
+  minY=300
+  maxY=100
+  const enemy = new Enemy(1,-300,Math.floor(Math.random() * (maxY - minY) + minY));
+  allEnemies.push(enemy);
+};
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
